dfb63daf8819b73b0c55affdeee2387409e51872
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2024-07-30 14:17:31 +0530

    updated api endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi import Body
 from transformers import AutoProcessor, SeamlessM4Tv2Model
 from typing import List, Dict
 import json
@@ -15,7 +16,7 @@ model = SeamlessM4Tv2Model.from_pretrained(model_path)
 
 
 @app.post("/translate")
-async def translate(sentences: List[str], src_lang: str, tgt_lang: str) -> Dict[str, List[str]]:
+async def translate(sentences: List[str], src_lang: str=Body(default='cmn'), tgt_lang: str=Body(default='eng')) -> Dict[str, List[str]]:
     translations = []
     for sentence in sentences:
         inputs = processor(text=sentence, src_lang=src_lang, return_tensors="pt")
